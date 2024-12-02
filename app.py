2dd00d6c2986719af4a08fc18cbb5decf60b043d
yeopster <156521948+yeopster@users.noreply.github.com>
2024-12-02 13:02:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import numpy as np
+import requests
 import pickle
 
-model = pickle.load(open('project\saved_steps.pkl', 'rb'))
+url = "https://github.com/yeopster/project/blob/main/saved_steps.pkl"
+
+response = requests.get(url)
+
+with open('saved_steps.pkl', 'wb') as file:
+     file.write(response,content)
+
+with open('saved_steps.pkl', 'rb') as file:
+      model=pickel.load(file)
 
 # Streamlit app
 st.title("Prediction of Cannula Distorted")
