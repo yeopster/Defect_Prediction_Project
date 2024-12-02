636a7f04a4de7676aa668a33f7b753b3db28702b
yeopster <156521948+yeopster@users.noreply.github.com>
2024-12-02 14:44:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,5 @@ import numpy as np
 import requests
 import pickle
 
-with open('saved_steps.pkl', 'rb') as file:
-  model = pickle.load(file)
 # Streamlit app
 st.title("Prediction of Cannula Distorted")
